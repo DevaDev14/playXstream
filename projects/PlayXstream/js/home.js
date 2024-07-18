99a4e49947e66c95a1e377ab0521e0cdcb012035
DevaDev <131745222+DevaDev14@users.noreply.github.com>
2024-07-18 10:59:24 +0530

    Add files via upload

---
+++ projects/PlayXstream/js/home.js
@@ -0,0 +1,6 @@
+const loginForm = document.getElementById('login-form');
+const loginButton = document.getElementById('login-button');
+
+loginButton.addEventListener('click', function(event) {
+  window.location.href="http://127.0.0.1:5500/HTML/home.html";
+});
